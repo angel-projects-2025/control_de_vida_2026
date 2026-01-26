31ac436528da0fc10aa01de504bd11bec0734538
Angel Lezama <lezamaangel8@gmail.com>
2026-01-25 21:45:02 -0500

    Ajustes responsive para móvil y botón meses

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const btnMeses = document.getElementById("btnMeses");
+    const listaMeses = document.getElementById("listaMeses");
+
+    console.log("JS cargado"); // 👈 prueba
+
+    btnMeses.addEventListener("click", function () {
+        console.log("Click en Meses"); // 👈 prueba
+        listaMeses.classList.toggle("mostrar");
+    });
+});
